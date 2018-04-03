edf6785c4c5dfe853c58f4909a04cd961fd9b99f
Paulie-K <36704572+Paulie-K@users.noreply.github.com>
2018-04-03 14:26:00 +0200

    Add files via upload

---
+++ MathLib.cc
@@ -0,0 +1,5 @@
+// Pokud použijete jiné jméno souboru, je potřeba ho změnit i v CMakeLists.txt i v MathTests.cc
+
+void Sum(){
+	;
+}
---
+++ MathLib.h
@@ -0,0 +1,3 @@
+// Pokud použijete jiné jméno souboru, je potřeba ho změnit i v CMakeLists.txt i v MathTests.cc
+
+void Sum(){};
