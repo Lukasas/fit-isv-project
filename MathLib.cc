3a6d2f577e984433f23c42d586e3bdb8b2892315
xjanik20 <luo96@seznam.cz>
2018-04-16 01:45:16 +0200

    First prototype of mathematical library

--- MathLib.cc
+++ MathLib.cc
@@ -1,8 +1,117 @@
-// Pokud použijete jiné jméno souboru, je potřeba ho změnit i v CMakeLists.txt i v MathTests.cc
-#include "MathLib.h"
+/**
+*	@file ivs_math.cpp
+*	@author Roman Janik (xjanik20)
+*	@brief IVS projekt 2 - Kalkulacka
+*/
 
+#include "ivs_math.h"
 
-// Ukazka aby kompilace nehazela error.
-int Sum(int a, int b){
-	return a + b;
+double opAdd(const double a, const double b)
+{
+	try
+	{
+		return a + b;
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+
+double opSubtract(const double a, const double b)
+{
+	try
+	{
+		return a - b;
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+
+double opMultiply(const double a, const double b)
+{
+	try
+	{
+		return a * b;
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+
+double opDivide(const double a, const double b)
+{
+	try
+	{
+		return a / b;
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+
+unsigned long long opFactorial(const unsigned long long a)
+{
+	unsigned long long i, factorial = 1;
+
+	try
+	{
+		/*if (a < 0)
+			throw out_of_range("out_of_range");*/
+
+		if (a == 0)
+			return factorial;
+
+		for (i = 1; i <= a; i++)
+		{
+			factorial *= i;
+		}
+
+		return factorial;
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+double opExponentiation(const double base, const int exponent)
+{
+	try
+	{
+		if ((base == 0) && (exponent == 0)) // 0^0 = 1
+			return 1;
+
+		return pow(base, exponent);
+
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+double opNthRoot(const double radicand, const double index)
+{
+	try
+	{
+		return pow(radicand, 1.0 / index);
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
+}
+double opSinus(const double a)
+{
+	try
+	{
+		return sin(a);
+	}
+	catch (const exception &e)
+	{
+		cout << e.what() << '\n';
+	}
 }
--- MathLib.h
+++ MathLib.h
@@ -1,3 +1,27 @@
-// Pokud použijete jiné jméno souboru, je potřeba ho změnit i v CMakeLists.txt i v MathTests.cc
+/**
+ *	@file ivs_math.h
+ *	@author Roman Janik (xjanik20)
+ *	@brief IVS projekt 2 - Kalkulacka
+ */
 
-int Sum(int a, int b);
+#ifndef IVS_MATH_H
+#define IVS_MATH_H
+
+#include <cmath>
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+
+// functions
+double opAdd(const double a, const double b);
+double opSubtract(const double a, const double b);
+double opMultiply(const double a, const double b);
+double opDivide(const double a, const double b);
+unsigned long long opFactorial(const unsigned long long a);
+double opExponentiation(const double base, const int exponent);
+double opNthRoot(const double radicand, const double index);
+double opSinus(const double a);
+
+#endif // !IVS_MATH_H
