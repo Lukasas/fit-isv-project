a3adde7c493b7e41070e37d33cd32d9e277c5608
Paulie-K <36704572+Paulie-K@users.noreply.github.com>
2018-04-22 09:39:17 +0200

    Přesunuto jinam.

--- MathLib.cc
+++
@@ -1,8 +0,0 @@
-// Pokud použijete jiné jméno souboru, je potřeba ho změnit i v CMakeLists.txt i v MathTests.cc
-#include "MathLib.h"
-
-
-// Ukazka aby kompilace nehazela error.
-int Sum(int a, int b){
-	return a + b;
-}
