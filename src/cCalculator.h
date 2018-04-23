9419cad225670dc4cb6cd356e6c42e6c37fc2d33
Lukasas <lukaschabek@gmail.com>
2018-04-23 13:31:32 +0200

    Added documentation for functions in cCalculator and mainwindow

--- src/cCalculator.h
+++ src/cCalculator.h
@@ -16,17 +16,50 @@ private:
 public:
 	cCalculator();
 	~cCalculator();
-
+    /**
+     * @brief memorySum
+     * Function that sums memory with value.
+     * @param Value that'll be added into memory.
+     */
 	void memorySum(const double value);
+
+    /**
+     * @brief memorySub
+     * Function that subtracts memory with number.
+     * @param Value that will be subtracted from memory value.
+     */
 	void memorySub(const double value);
+
+    /**
+     * @brief memoryRecall
+     * Function that returns current value in memory.
+     * @return Value that is stored in memory.
+     */
 	double memoryRecall() const;
+
+    /**
+     * @brief memoryClean
+     * Function that sets memory value to 0.0
+     */
 	void memoryClean();
 
 	 
+    /**
+     * @brief fnCall
+     * This function accepts value with function name for calculations.
+     * @param value from display already in double
+     * @param fnName Name of a function in specific format "SUM", "SUB" ...
+     * @return Current value in calculator
+     */
 	double fnCall(const double value, const std::string fnName);
 
+
 	double Result() const;
-	void Clean();
+    /**
+     * @brief Clean
+     * Resets values in calculator.
+     */
+    void Clean();
 
 };
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -28,10 +28,31 @@ public:
         POW,
         SQRT
     };
-
+    /**
+     * @brief GetDisplayNumber
+     * Returns value from display already converted into double.
+     * @param ok Serves as an test if the conversion was successful.
+     * @return Numeric value on display.
+     */
     double GetDisplayNumber(bool & ok) const;
+    /**
+     * @brief SetDisplayNumber
+     * Sets display text to entered value.
+     * @param value Number that will be printed on display.
+     */
     void SetDisplayNumber(double value);
+    /**
+     * @brief SetDisplayText
+     * Sets display with input text.
+     * @param str Text that will be printed on the display.
+     */
     void SetDisplayText(QString str);
+
+    /**
+     * @brief GetDisplayText
+     * Gets text that is currently on display.
+     * @return Text from display.
+     */
     QString GetDisplayText() const;
 private slots:
 //    void on_pushButton_5_clicked();
@@ -41,6 +62,10 @@ private slots:
 private:
     Ui::MainWindow *ui;
     QLineEdit * display;
+    /**
+     * @brief connectButtons
+     * Function that connects buttons signal with slots.
+     */
     void connectButtons(QPushButton *);
     cCalculator c;
 };
